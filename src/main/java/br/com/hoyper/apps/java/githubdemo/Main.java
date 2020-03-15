b92aac886cdb7d7bd407257e31b5a48e86f7b983
edy <edleyrocha@hotmail.com>
2020-03-15 11:13:34 -0400

    Rollback Main Teste

--- src/main/java/br/com/hoyper/apps/java/githubdemo/Main.java
+++ src/main/java/br/com/hoyper/apps/java/githubdemo/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("GitHubDemo.fxml"));
-			Scene scene = new Scene(root,root.getPrefWidth(),root.getPrefHeight());
+			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Git Hub Demo");
 			primaryStage.setScene(scene);
